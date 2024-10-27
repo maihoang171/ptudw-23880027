6ad651da1db96efa6b11b842b83a7ad6952aff27
Jayce <maihoang171@gmail.com>
2024-10-27 11:08:41 +0700

    using redis

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const expressHbs = require("express-handlebars");
 const { createStarList } = require("./controllers/handleBarsHelpers")
 const { createPagination } = require("express-handlebars-paginate");
 const session = require("express-session");
+const redisStore = require('connect-redis').default;
+const { createClient } = require('redis');
+const redisClient = createClient({
+    //url: 'rediss://red-cserl53tq21c738g71ug:uYtYiVDUKBO4LkV8YHRGmOzN2hPyECc7@oregon-redis.render.com:6379'
+    url: "redis://red-cserl53tq21c738g71ug:6379"
+})
+redisClient.connect().catch(console.error)
 
 app.use(express.static(__dirname + "/html"));
 
@@ -26,6 +33,7 @@ app.set("view engine", "hbs")
 
 app.use(session({
     secret: 'S3cret', //Phat sinh Id co tung session
+    store: new redisStore({ client: redisClient }),
     resave: false,
     saveUninitialized: false,
     cookie: {
